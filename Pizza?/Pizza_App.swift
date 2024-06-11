842664c05cfb8bbea5c74c4a19f8169f760a1363
Igor Łopatka <63002337+igorlopatka@users.noreply.github.com>
2024-06-11 17:34:22 +0200

    Initial Commit

---
+++ Pizza?/Pizza_App.swift
@@ -0,0 +1,17 @@
+//
+//  Pizza_App.swift
+//  Pizza?
+//
+//  Created by Igor Łopatka on 11/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Pizza_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
